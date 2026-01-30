f967683b75337f5611e7713c7aba7e2758622e8d
Mohamed Hany <xspeedo909@gmail.com>
2026-01-30 05:52:06 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+# إعدادات JSONBin
+JSONBIN_BIN_ID = "697c129dae596e708f02274d"  # استبدل بمعرف البن الخاص بك
+JSONBIN_API_KEY = "$2a$10$EVEgnKcuZEoujGa1B3HQmuU7C3Eh6NfJFdYR9IH0r1mQR3UAc2SU2"  # استبدل بمفتاح API الخاص بك
+JSONBIN_URL = f"https://api.jsonbin.io/v3/b/{JSONBIN_BIN_ID}"
+
+# إعدادات التطبيق
+APP_TITLE = "لوحة تحكم أكاديمية Alif-Lam-Meem"
+APP_VERSION = "1.0.0"
+COMPANY_NAME = "أكاديمية Alif-Lam-Meem لتحفيظ القرآن الكريم بالتجويد"
+MANAGER_NAME = "أ. سارة بحيري"
+MANAGER_PHONE = "+20 10 6833 9196"
+MANAGER_EMAIL = "info@alif-lam-meem.com"
